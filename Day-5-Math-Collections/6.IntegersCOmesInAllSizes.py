8265d02a54c932e1f27dd524a8f7aed3c08ea05d
Ashutosh Mahawar <ashutosh.mahawar11@gmail.com>
2021-01-13 13:04:21 +0530

    Create 6.IntegersCOmesInAllSizes.py

---
+++ Day-5-Math-Collections/6.IntegersCOmesInAllSizes.py
@@ -0,0 +1,12 @@
+"""
+Task
+Read four numbers, a, b, c, and d, and print the result of a^b + c^d.
+
+"""
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(a**b + c**d)
