54bc0b137aa6927d4972402601a0b8d78636be48
Ashutosh Mahawar <ashutosh.mahawar11@gmail.com>
2021-01-13 13:01:44 +0530

    Create 4.Mod_DIvMod.py

---
+++ Day-5-Math-Collections/4.Mod_DIvMod.py
@@ -0,0 +1,16 @@
+"""
+Task
+Read in two integers,  a and b , and print three lines.
+The first line is the integer division  (While using Python2 remember to import division from __future__).
+The second line is the result of the modulo operator: .
+The third line prints the divmod of a and b.
+"""
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+if __name__ =="__main__":
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a%b)
+    print(divmod(a,b))
